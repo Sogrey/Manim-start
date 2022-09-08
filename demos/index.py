5f220a944690970c72c8e1110ca31355e9870755
sogrey <suzhaoheng2006@163.com>
2022-09-09 02:02:54 +0800

    update

--- demos/index.py
+++ demos/index.py
@@ -1,4 +1,5 @@
 from manim import *
+# manim titles_credits.py -pqm
 
 # 导入base目录下基础工具函数
 import sys
@@ -17,7 +18,6 @@ def Logo(self):
     self.add(circle, square, triangle)
     self.wait(3)
 
-# manim titles_credits.py -pqm
 class ShowLogo(Scene):
     def construct(self):
         Title(self, "The logo of the manim")
