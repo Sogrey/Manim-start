c7c24a957030dd30816a5e5f81b970bc80faa398
sogrey <suzhaoheng2006@163.com>
2022-09-09 01:02:30 +0800

    add titles_credits.py

---
+++ titles_credits.py
@@ -0,0 +1,27 @@
+from manim import *
+
+def Title(self, title):
+    title = Text(title)
+    self.add(title)
+    self.wait(3)
+    self.play(ScaleInPlace(title, 0.5))
+    title.generate_target()
+    title.target.shift(UL*3.8)
+    self.play(MoveToTarget(title))
+    self.wait(3)
+
+def Credits(self, credits=''):
+    self.clear()
+    if len(credits) == 0:
+        credits = "THE END"
+    text = Text(credits)
+    boundary = AnimatedBoundary(text, colors=[YELLOW], cycle_rate=3)
+    self.add(text, boundary)
+    self.wait(2)
+
+# manim titles_credits.py -pqm
+class TestTitle(Scene):
+    def construct(self):
+        Title(self, 'The title of the animation')
+
+        Credits(self,'THE END...')
