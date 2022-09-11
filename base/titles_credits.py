91187cb9c376f8bbedda662d6680b0deb761d560
sogrey <suzhaoheng2006@163.com>
2022-09-11 20:15:23 +0800

    update

--- base/titles_credits.py
+++ base/titles_credits.py
@@ -1,22 +1,30 @@
 from manim import *
 
-def Title(self, title, offsetMultiple=3.5):
-    title = Text(title)
-    self.add(title)
+def Title(self, title):
+    title0 = Text(title) # 初态
+    title = Text(title) # 终态
+    self.add(title0)
     self.wait(3)
-    title.generate_target()
-    title.target.shift(UL*offsetMultiple).scale(0.5)
-    # ScaleInPlace(title, 0.5)
-    self.play(MoveToTarget(title))
+    title.to_corner(UP + LEFT).scale(0.5)
+    self.play(Transform(title0, title)) # 过渡动画
     self.wait(2)
 
 def Credits(self, credits=''):
     self.clear()
     if len(credits) == 0:
         credits = "THE END"
-    text = Text(credits).set_color(TEAL_D).scale(1)
-    self.add(text)
+    # text = Text(credits).set_color(TEAL_D).scale(1).move_to(DOWN)
+    # self.play(FadeIn(text, shift=DOWN, scale=0.66))
+    # self.wait(2)
+    # self.play(FadeOut(text, shift=DOWN * 2, scale=1.5))
+
+    tex_in = Tex("THE END").scale(1).set_color(TEAL_D)
+    tex_out = Tex("@Sogrey").scale(.5).set_color(TEAL_D)
+    self.play(FadeIn(tex_in, shift=DOWN, scale=0.66))
     self.wait(2)
+    self.play(ReplacementTransform(tex_in, tex_out))
+    self.wait(2)
+    self.play(FadeOut(tex_out, shift=DOWN * 2, scale=1.5))
 
 # manim titles_credits.py -pqm
 class TestTitle(Scene):
--- demos/index.py
+++ demos/index.py
@@ -18,10 +18,55 @@ def Logo(self):
     self.add(circle, square, triangle)
     self.wait(3)
 
+def mul9x9(self):
+    square = Square()
+    self.add(square)
+    self.wait(3)
+
 class ShowLogo(Scene):
     def construct(self):
         Title(self, "The logo of the manim")
 
-        Logo(self)
+        # Logo(self)
+        # mul9x9(self)
 
         Credits(self)
+
+
+class HelloCircle(Scene):
+    def construct(self):
+        # blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        # We can also create a "plain" circle and add the desired attributes via set methods:
+        circle = Circle()
+        blue_circle = circle.set_color(BLUE).set_opacity(0.5)
+        
+        label = Text("A wild circle appears!")
+        label.next_to(blue_circle, DOWN, buff=0.5)
+        
+        self.play(Create(blue_circle), Write(label))
+        self.wait()
+
+class CircleAnnouncement(Scene):
+    def construct(self):
+        blue_circle = Circle(color=BLUE, fill_opacity=0.5)
+        announcement = Text("Let us draw a circle.")
+        
+        self.play(Write(announcement))
+        self.wait()
+        
+        self.play(announcement.animate.next_to(blue_circle, UP, buff=0.5))
+        self.play(Create(blue_circle))
+
+class AnimateSyntax(Scene):
+    def construct(self):
+        triangle = Triangle(color=RED, fill_opacity=1)
+        self.play(DrawBorderThenFill(triangle))
+        self.play(triangle.animate.shift(LEFT))
+        self.play(triangle.animate.shift(RIGHT).scale(2))
+        self.play(triangle.animate.rotate(PI/3))
+
+class CauchyIntegralFormula(Scene):
+    def construct(self):
+        formula = MathTex(r"[z^n]f(z) = \frac{1}{2\pi i}\oint_{\gamma} \frac{f(z)}{z^{n+1}}~dz")
+        self.play(Write(formula), run_time=3)
+        self.wait()
